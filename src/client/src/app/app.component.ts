03935fe9d2d36825c9c09c9e2f542f4f64666bdd
Makhliyo Ablakulova <mablaqulova@gmail.com>
2021-09-03 23:02:47 +0500

    nav-bar added

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Catering';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/foodshops').subscribe((response: any) => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
