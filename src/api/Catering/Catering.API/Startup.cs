4ca029473a756e9683aa475c29fc988ae2469e61
Makhliyo Ablakulova <mablaqulova@gmail.com>
2021-09-08 20:03:53 +0500

    static files added

--- src/api/Catering/Catering.API/Startup.cs
+++ src/api/Catering/Catering.API/Startup.cs
@@ -17,11 +17,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -118,14 +120,24 @@ namespace Catering.API
             {
                 app.UseHsts();
             }
+            
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuth();
+            app.UseStaticFiles();
 
-            app.UseCors("CorsPolicy");
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "Content")
+                ),
+                RequestPath = "/content"
+            });
+
+            app.UseAuth();
 
             app.UseEndpoints(endpoints =>
             {
